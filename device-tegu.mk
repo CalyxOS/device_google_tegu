b1f35705c393b0bc55fdffd572d9ce7fa530c748
Chirayu Desai <chirayudesai1@gmail.com>
2021-10-28 17:37:57 +0530

    tegu: Add privapp permissions for EuiccGoogle
    Co-authored-by: Michael Bestas <mkbestas@gmail.com>
    Change-Id: I093cd52996b11b88343c474e85d34595b10f5c62

--- device-tegu.mk
+++ device-tegu.mk
@@ -21,6 +21,10 @@ SHIPPING_API_LEVEL := 35
 # Inherit from zumapro
 include device/google/zumapro/device-shipping-common.mk
 
+# EUICC
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/permissions/permissions_com.google.android.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/permissions_com.google.android.euicc.xml
+
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.allocator@1.0-service \
