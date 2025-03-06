4e86b70cf6d5439089590892a5401cade7111cd4
Michael Bestas <mkbestas@gmail.com>
2025-03-06 23:31:19 +0200

    tegu: Include missing vibrator sepolicy
    Change-Id: Iac3a36e884dc1747d4fd0a60b418ffbf3779ffdb

--- tegu-sepolicy.mk
+++ tegu-sepolicy.mk
@@ -2,3 +2,5 @@
 BOARD_SEPOLICY_DIRS += device/google/tegu-sepolicy/vendor
 BOARD_SEPOLICY_DIRS += device/google/tegu-sepolicy/tracking_denials
 
+BOARD_VENDOR_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/vibrator/common
+BOARD_VENDOR_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/vibrator/cs40l26
