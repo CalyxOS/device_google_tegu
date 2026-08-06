79f5b722f0592724830c79eb596b19d6c1b9d81b
Michael Bestas <mkbestas@gmail.com>
2026-08-07 00:08:14 +0300

    calyx: Update to CP2A.260805.005
    Change-Id: Ib56e4cb4dcbbb99a6e0203b04de483fc5dc16130

--- calyx_tegu.mk
+++ calyx_tegu.mk
@@ -20,9 +20,9 @@ PRODUCT_SYSTEM_MANUFACTURER := Google
 PRODUCT_SYSTEM_NAME := generic_system_google
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="tegu-user 17 CP2A.260705.006 15641320 release-keys" \
-    BuildFingerprint=google/tegu/tegu:17/CP2A.260705.006/15641320:user/release-keys \
-    BuildSystemFingerprint=google/generic_system_google/generic:17/CP2A.260705.006/15641320:user/release-keys \
+    BuildDesc="tegu-user 17 CP2A.260805.005 15828068 release-keys" \
+    BuildFingerprint=google/tegu/tegu:17/CP2A.260805.005/15828068:user/release-keys \
+    BuildSystemFingerprint=google/generic_system_google/generic:17/CP2A.260805.005/15828068:user/release-keys \
     DeviceProduct=$(DEVICE_CODENAME)
 
 PRODUCT_RESTRICT_VENDOR_FILES := false
