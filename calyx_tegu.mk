a4a324707908c074247ea687756e6f7079e82985
Michael Bestas <mkbestas@gmail.com>
2026-08-11 20:42:42 +0300

    fixup! Initial CalyxOS makefile
    Change-Id: Iac12bc9989a302b0bade78a2e7e08c3e88b94610

--- calyx_tegu.mk
+++ calyx_tegu.mk
@@ -1,30 +1,15 @@
 #
-# SPDX-FileCopyrightText: The LineageOS Project
 # SPDX-FileCopyrightText: The Calyx Institute
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit from lineage device
+$(call inherit-product, device/google/tegu/lineage_tegu.mk)
+
 # Inherit some common stuff
 $(call inherit-product, vendor/calyx/config/common_phone.mk)
 
-# Inherit device configuration
-DEVICE_CODENAME := tegu
-DEVICE_PATH := device/google/tegu
-VENDOR_PATH := vendor/google/tegu
-$(call inherit-product, $(DEVICE_PATH)/aosp_$(DEVICE_CODENAME).mk)
-
 # Device identifier. This must come after all inclusions
 PRODUCT_NAME := calyx_$(DEVICE_CODENAME)
-PRODUCT_SYSTEM_BRAND := google
-PRODUCT_SYSTEM_MANUFACTURER := Google
-PRODUCT_SYSTEM_NAME := generic_system_google
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="tegu-user 17 CP2A.260805.005 15828068 release-keys" \
-    BuildFingerprint=google/tegu/tegu:17/CP2A.260805.005/15828068:user/release-keys \
-    BuildSystemFingerprint=google/generic_system_google/generic:17/CP2A.260805.005/15828068:user/release-keys \
-    DeviceProduct=$(DEVICE_CODENAME)
 
 PRODUCT_RESTRICT_VENDOR_FILES := false
-
-$(call inherit-product, $(VENDOR_PATH)/$(DEVICE_CODENAME)-vendor.mk)
